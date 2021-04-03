13d0acef688c421fef39e3fa69bdf3bc92a4d7d2
jakemperry <Jakemperry@gmail.com>
2021-04-03 16:20:42 -0700

    get bearings on app

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Plotly Challenge
+
+### PseudoCode
+1. Use the D3 Library to read in ```samples.json```
--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,40 @@
-var samples = fetch('samples.json')
-    .then(response => response.json())
-    .then(jsonResponse => console.log(jsonResponse))
 
-console.log(samples)
+// On change to the DOM, call getData()
+d3.selectAll("#selDataset").on("change", getData);
+
+// Function called by DOM changes
+function getData() {
+  var dropdownMenu = d3.select("#selDataset");
+  // Assign the value of the dropdown menu option to a variable
+  var dataset = dropdownMenu.property("value");
+  // Initialize an empty array for the country's data
+  var data = [];
+
+  if (dataset == 'us') {
+      data = us;
+  }
+  else if (dataset == 'uk') {
+      data = uk;
+  }
+  else if (dataset == 'canada') {
+      data = canada;
+  }
+  // Call function to update the chart
+  updatePlotly(data);
+}
+
+// Update the restyled plot's values
+function updatePlotly(newdata) {
+  Plotly.restyle("pie", "values", [newdata]);
+}
+
+
+function horizontalBar(data){d3.json('samples.json').then(function show(data){
+    console.log(data)
+    
+
+    });
+
+}
+
+horizontalBar()
