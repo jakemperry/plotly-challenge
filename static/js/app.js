34cc2115aebdd1c9807ce32da00638187d69f55e
jakemperry <Jakemperry@gmail.com>
2021-04-04 09:10:59 -0700

    add titls and axis labels to all charts

--- static/js/app.js
+++ static/js/app.js
@@ -63,8 +63,19 @@ function loadCharts() {
         orientation: 'h' //This makes the chart a horizontal bar chart
       }];
 
+    var layout={
+        title:  { text: `Top 10 OTU for Test Subject No. ${currentID}`, font: { size: 24 } },
+        xaxis: {
+            title: 'OTU Count'
+        },
+        yaxis: {
+            title: 'OTU ID'
+        },
+        font: { color: "black", family: "Arial" }
+    }
+
     //Plot the horizontal bar chart
-    Plotly.newPlot('bar', hbarData);
+    Plotly.newPlot('bar', hbarData, layout);
 
     //Make an array of bubble sizes.  Exponent of 1.7 to allow larger
     //bacteria counts to have significantly larger bubble sizes
@@ -85,8 +96,19 @@ function loadCharts() {
         }
     }];
 
+    var bubbleLayout = {
+        title: {text : `All OTU Present for Test Subject No. ${currentID}`, font: {size: 24}},
+        xaxis: {
+            title: 'OTU ID Number'
+        },
+        yaxis: {
+            title: 'OTU Count'
+        },
+        font: {color: "black", family: "Arial"}
+    }
+
     //Plot the bubble chart
-    Plotly.newPlot('bubble', bubbles);
+    Plotly.newPlot('bubble', bubbles, bubbleLayout);
     
     //Get metadata that matches current individual ID
     currentMeta = samplesData.metadata.filter(filterMeta)
@@ -112,52 +134,6 @@ function loadCharts() {
         <br>${metaKeys[6]}: ${metaValues[6]}</p>`)
 }
 
-// function loadGauge(){
-
-//     var data = [
-//         {
-//           type: "indicator",
-//           mode: "gauge+number+delta",
-//           value: metaValues[6],
-//           title: { text: "Belly Button Washing Frequency", font: { size: 24 } },
-//           gauge: {
-//             axis: { range: [null, 9.5], nticks: 10, tickwidth: 1, tickcolor: "black" },
-//             bar: { color: "black" },
-//             bgcolor: "white",
-//             borderwidth: 2,
-//             bordercolor: "black",
-//             steps: [
-//               { range: [0, 0.5], color: "red", text:"0", line: {color: "black", width: 1}},
-//               { range: [0.5, 1.5], color: "red" , line: {color: "black", width: 1}},
-//               { range: [1.5, 2.5], color: "orange", line: {color: "black", width: 1} },
-//               { range: [2.5, 3.5], color: "orange", line: {color: "black", width: 1} },
-//               { range: [3.5, 4.5], color: "yellow" , line: {color: "black", width: 1}},
-//               { range: [4.5, 5.5], color: "yellow" , line: {color: "black", width: 1}},
-//               { range: [5.5, 6.5], color: "green" , line: {color: "black", width: 1}},
-//               { range: [6.5, 7.5], color: "green" , line: {color: "black", width: 1}},
-//               { range: [7.5, 8.5], color: "green" , line: {color: "black", width: 1}},
-//               { range: [8.5, 9.5], color: "green" , line: {color: "black", width: 1}}
-//             ],
-//             threshold: {
-//               line: { color: "black", width: 4 },
-//               thickness: 0.75,
-//               value: 9
-//             }
-//           }
-//         }
-//       ];
-      
-//       var layout = {
-//         width: 500,
-//         height: 400,
-//         margin: { t: 25, r: 25, l: 25, b: 25 },
-//         paper_bgcolor: "white",
-//         font: { color: "black", family: "Arial" }
-//       };
-      
-//       Plotly.newPlot('gauge', data, layout);
-//     }
-
 //Load up the data
 d3.json('samples.json').then(function init(data){
 
@@ -182,9 +158,6 @@ d3.json('samples.json').then(function init(data){
     // bob();
 })
 
-
-    
-    // loadGauge() 
 
 //Set an event handler for any time the dropdown menu selection changes.  This will re-run loadCharts()
 d3.selectAll("#selDataset").on("change", function runAll(){
