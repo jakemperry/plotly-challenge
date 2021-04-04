cd813aee814da3504cbd1b0b4575f06bf8db3147
jakemperry <Jakemperry@gmail.com>
2021-04-03 17:44:11 -0700

    get the current id when changing the dropdown selection, updates a variable

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 //1. Update the dropdown menu with all of the individual ids.
 function populateDropDown(){
     d3.json('samples.json').then(function updateList(data){
-        console.log(data.names)
+        // console.log(data)
         d3.select('#selDataset').selectAll('option')
             .data(data.names)
                 .enter()
@@ -11,34 +11,45 @@ function populateDropDown(){
     })
 }
 populateDropDown()
-// // On change to the DOM, call getData()
-// d3.selectAll("#selDataset").on("change", getData);
 
-// // Function called by DOM changes
-// function getData() {
-//   var dropdownMenu = d3.select("#selDataset");
-//   // Assign the value of the dropdown menu option to a variable
-//   var dataset = dropdownMenu.property("value");
-//   // Initialize an empty array for the country's data
-//   var data = [];
+var currentID=""
+var samplesData = []
+var currentSample = []
+d3.json('samples.json').then(function loadData(d){
+    samplesData = d;
+})
 
-//   if (dataset == 'us') {
-//       data = us;
-//   }
-//   else if (dataset == 'uk') {
-//       data = uk;
-//   }
-//   else if (dataset == 'canada') {
-//       data = canada;
-//   }
-//   // Call function to update the chart
-//   updatePlotly(data);
-// }
+function filterSamples(sample){
+    return sample.id === currentID;
+}
+//2. Create a horizontal bar chart with the selected ID
 
-// // Update the restyled plot's values
-// function updatePlotly(newdata) {
-//   Plotly.restyle("pie", "values", [newdata]);
-// }
+
+// On change to the DOM, call getData()
+d3.selectAll("#selDataset").on("change", getData);
+
+// Function called by DOM changes
+function getData() {
+  var dropdownMenu = d3.select("#selDataset");
+  // Assign the value of the dropdown menu option to a variable
+  currentID = dropdownMenu.property("value");
+  console.log(currentID)
+  currentSample = samplesData.samples.filter(filterSamples)
+}
+
+var exampledata = [{
+    type: 'bar',
+    x: [20, 14, 23],
+    y: ['giraffes', 'orangutans', 'monkeys'],
+    orientation: 'h'
+  }];
+  
+  Plotly.newPlot('bar', exampledata);
+
+// Update the restyled plot's values
+function updatePlotly(newdata) {
+  Plotly.restyle("pie", "values", [newdata]);
+}
 
 
 // function horizontalBar(data){d3.json('samples.json').then(function show(data){
