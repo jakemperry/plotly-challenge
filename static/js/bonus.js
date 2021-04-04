eecaa1bbb25b9c1e15d8dea43142ea7feb039d99
jakemperry <Jakemperry@gmail.com>
2021-04-04 09:34:02 -0700

    update comments in bonus.js

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,49 +1,52 @@
+//Create a function to build the gauge
 function loadGauge(){
+  //Set up gauge data, based on example from Plotly
+  var data = [
+      {
+        type: "indicator",
+        mode: "gauge+number", 
+        value: metaValues[6],  //Loaded from the current set of subject metadata
+        title: { text: "Belly Button Washing Frequency", font: { size: 24 } },
+        gauge: {
+          // Make 10 ticks, one for each number 0 through 9
+          axis: { range: [null, 9.5], nticks: 10, tickwidth: 1, tickcolor: "black" },
+          bar: { color: "purple" },
+          bgcolor: "white",
+          borderwidth: 2,
+          bordercolor: "black",
+          // Add steps to show gradual change in color as the gauge value increases.  Outlines
+          // on the steps give a cleaner look
+          steps: [
+            { range: [0, 0.5], color: "red", text:"0", line: {color: "black", width: 1}},
+            { range: [0.5, 1.5], color: "red" , line: {color: "black", width: 1}},
+            { range: [1.5, 2.5], color: "orange", line: {color: "black", width: 1} },
+            { range: [2.5, 3.5], color: "orange", line: {color: "black", width: 1} },
+            { range: [3.5, 4.5], color: "yellow" , line: {color: "black", width: 1}},
+            { range: [4.5, 5.5], color: "yellow" , line: {color: "black", width: 1}},
+            { range: [5.5, 6.5], color: "green" , line: {color: "black", width: 1}},
+            { range: [6.5, 7.5], color: "green" , line: {color: "black", width: 1}},
+            { range: [7.5, 8.5], color: "green" , line: {color: "black", width: 1}},
+            { range: [8.5, 9.5], color: "green" , line: {color: "black", width: 1}}
+          ],
 
-var data = [
-    {
-      type: "indicator",
-      mode: "gauge+number",
-      value: metaValues[6],
-      title: { text: "Belly Button Washing Frequency", font: { size: 24 } },
-      gauge: {
-        axis: { range: [null, 9.5], nticks: 10, tickwidth: 1, tickcolor: "black" },
-        bar: { color: "purple" },
-        bgcolor: "white",
-        borderwidth: 2,
-        bordercolor: "black",
-        steps: [
-          { range: [0, 0.5], color: "red", text:"0", line: {color: "black", width: 1}},
-          { range: [0.5, 1.5], color: "red" , line: {color: "black", width: 1}},
-          { range: [1.5, 2.5], color: "orange", line: {color: "black", width: 1} },
-          { range: [2.5, 3.5], color: "orange", line: {color: "black", width: 1} },
-          { range: [3.5, 4.5], color: "yellow" , line: {color: "black", width: 1}},
-          { range: [4.5, 5.5], color: "yellow" , line: {color: "black", width: 1}},
-          { range: [5.5, 6.5], color: "green" , line: {color: "black", width: 1}},
-          { range: [6.5, 7.5], color: "green" , line: {color: "black", width: 1}},
-          { range: [7.5, 8.5], color: "green" , line: {color: "black", width: 1}},
-          { range: [8.5, 9.5], color: "green" , line: {color: "black", width: 1}}
-        ],
-        threshold: {
-          line: { color: "black", width: 4 },
-          thickness: 0.75,
-          value: 9
+          // The threshold of the gauge shows a maximum value to expect from the gauge
+          threshold: {
+            line: { color: "black", width: 4 },
+            thickness: 0.75,
+            value: 9
+          }
         }
       }
-    }
   ];
-  
+    
   var layout = {
-    width: 500,
-    height: 400,
-    margin: { t: 25, r: 25, l: 25, b: 25 },
+    width: 450,
+    height: 300,
+    margin: { t: 80, r: 25, l: 25, b: 25 },
     paper_bgcolor: "white",
     font: { color: "black", family: "Arial" }
   };
-  
+    
+  // Add the chart to the DOM
   Plotly.newPlot('gauge', data, layout);
 }
-
-// loadGauge() 
-
-  // d3.select("#sample-metadata").on("change", loadGauge)
